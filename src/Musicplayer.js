fff8b3af68db347a131ee716cf6e52acbd9f9a97
mohanaprasathtech <mohanaprasath12345@gmail.com>
2023-12-21 19:39:43 +0530

    slider config

--- src/Musicplayer.js
+++ src/Musicplayer.js
@@ -1,21 +1,28 @@
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
-import AntDesign from 'react-native-vector-icons/AntDesign'
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Slider from '@react-native-community/slider';
 
 const Musicplayer = () => {
   return (
     <View style={styles.container}>
       <Text>Musicplayer</Text>
-      <AntDesign name="stepforward" size={280}/>
+      <AntDesign name="stepforward" size={20} />
+      <Slider
+        style={{width: 200, height: 40}}
+        minimumValue={0}
+        maximumValue={1}
+        minimumTrackTintColor="blue"
+        maximumTrackTintColor="red"
+      />
     </View>
   );
-
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems:'center',
+    alignItems: 'center',
   },
 });
 export default Musicplayer;
