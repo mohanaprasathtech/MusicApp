914e821216e3f5797a7b5156ad19c8f35db18a2a
mohanaprasathtech <mohanaprasath12345@gmail.com>
2023-12-21 19:34:24 +0530

    vectore icons

--- src/Musicplayer.js
+++ src/Musicplayer.js
@@ -1,10 +1,12 @@
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
 const Musicplayer = () => {
   return (
     <View style={styles.container}>
       <Text>Musicplayer</Text>
+      <AntDesign name="stepforward" size={280}/>
     </View>
   );
 
